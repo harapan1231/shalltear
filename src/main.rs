c39b52be3ffa9226acbced134055bfdbb4bb6eb3
harapan1231 <senang.harapan@gmail.com>
2017-12-22 16:17:05 +0900

    Use hyper

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+
+extern crate futures;
+extern crate tokio_core;
+extern crate hyper;
+
+use std::io::{self, Write};
+use futures::{Future, Stream};
+use tokio_core::reactor::Core;
+use hyper::Client;
+
 fn main() {
-    println!("Hello, world!");
+    let mut core = Core::new().unwrap();
+    let client = Client::new(&core.handle());
+
+    let uri = "http://httpbin.org/ip".parse().unwrap();
+    let work = client.get(uri).and_then(|res| {
+        println!("Response: {}", res.status());
+
+        res.body().for_each(|chunk| {
+            io::stdout()
+                .write_all(&chunk)
+                .map_err(From::from)
+        })
+    });
+    core.run(work).unwrap();
 }
