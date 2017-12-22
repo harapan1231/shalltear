9f9009cf63357ae90b777241bc8f4c70f1a93bd9
harapan1231 <senang.harapan@gmail.com>
2017-12-22 17:56:14 +0900

    Use hyper_tls

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,27 @@
 extern crate futures;
 extern crate tokio_core;
 extern crate hyper;
+extern crate hyper_tls;
 
 use std::io::{self, Write};
+
 use futures::{Future, Stream};
+
 use tokio_core::reactor::Core;
+
 use hyper::Client;
+use hyper_tls::HttpsConnector;
 
 fn main() {
     let mut core = Core::new().unwrap();
-    let client = Client::new(&core.handle());
+    let handle = core.handle();
 
-    let uri = "http://httpbin.org/ip".parse().unwrap();
+    let client = Client::configure()
+        .connector(HttpsConnector::new(4, &handle).unwrap())
+        .build(&handle);
+
+    let uri = "https://httpbin.org/ip".parse().unwrap();
+
     let work = client.get(uri).and_then(|res| {
         println!("Response: {}", res.status());
 
@@ -24,3 +34,4 @@ fn main() {
     });
     core.run(work).unwrap();
 }
+
